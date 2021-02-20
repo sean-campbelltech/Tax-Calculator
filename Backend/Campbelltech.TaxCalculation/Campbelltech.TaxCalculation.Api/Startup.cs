4e132862a4afac5d2a6c14f0b1ce286785853178
Sean Campbell <sean@campbelltech.co.za>
2021-02-20 21:29:17 +0200

    Added config to appsettings.json files

--- Backend/Campbelltech.TaxCalculation/Campbelltech.TaxCalculation.Api/Startup.cs
+++ Backend/Campbelltech.TaxCalculation/Campbelltech.TaxCalculation.Api/Startup.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using Campbelltech.TaxCalculation.Domain.Calculations;
+using Campbelltech.TaxCalculation.Domain.Configuration;
 using Campbelltech.TaxCalculation.Domain.Repositories;
 using Campbelltech.TaxCalculation.Domain.Services;
 using Microsoft.AspNetCore.Builder;
@@ -35,6 +36,7 @@ namespace Campbelltech.TaxCalculation.Api
         public void ConfigureServices(IServiceCollection services)
         {
             // service registration for DI
+            services.Configure<Config>(Configuration);
             services.AddScoped<IProgressiveTaxRateRepository, ProgressiveTaxRateRepository>();
             var taxCalculation = new TaxCalculationFactory().Create(services);
             services.AddTransient<ITaxCalculation>(x => taxCalculation);
@@ -54,7 +56,11 @@ namespace Campbelltech.TaxCalculation.Api
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// Used to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app">Defines a class that provides the mechanisms to configure an application's request pipeline.</param>
+        /// <param name="env">Provides information about the web hosting environment an application is running in.</param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
