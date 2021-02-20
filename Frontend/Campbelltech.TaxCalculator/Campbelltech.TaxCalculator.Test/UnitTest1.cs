10deef03d0923b752d07346fe0a448417b7a0e50
Sean Campbell <sean@campbelltech.co.za>
2021-02-20 10:16:50 +0200

    Generated bare bones projects for the frontend

---
+++ Frontend/Campbelltech.TaxCalculator/Campbelltech.TaxCalculator.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Campbelltech.TaxCalculator.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
